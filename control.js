66c855e172e52c2d4e1cd759093b3ca0727331ad
Smashboarder360 <51089589+Smashboarder360@users.noreply.github.com>
2021-03-09 23:16:01 -0500

    Added Starter Code

---
+++ control.js
@@ -0,0 +1,121 @@
+//arrays used
+let sessions = new Array(); //stores data about each sessions
+let type = new Array(); //stores data about all possiable session type
+
+//adds default types
+type.push("");
+type.push("options");
+
+
+let list = document.getElementById('list'); //maybe useless idk
+
+//used for stopwatch, clock stores the time
+let offset,clock,interval;
+
+//clock functions
+function start(){
+    if(!interval){
+        offset = Date.now();
+        interval = setInterval(update,100);
+    }
+    renderClock();
+}
+function stop(){
+    if (interval){
+        clearInterval(interval);
+        interval = null;
+    }
+    renderClock();
+}
+function reset(){
+    clock =0;
+    renderClock(0);
+}
+function update(){
+    clock += delta();
+    renderClock();
+}
+function delta(){
+    let now = Date.now();
+        d = now - offset;
+    offset = now;
+    return d;
+}
+
+//creates new type and adds it to type array
+function createNewType(){
+    let t = document.getElementById('types').value;
+    console.log(t);
+    if(t === 'options'){
+        let p = prompt("enter new type","");
+        if (p != null){
+            type.push(p);
+        }
+        render();
+
+    }
+}
+
+//renders the clock only
+function renderClock(){
+    let x = document.getElementById('demo').innerHTML = clock/1000;
+}
+
+//renders the the whole page
+function render(){
+    renderClock();
+    console.log(sessions);
+    
+    //updates select options
+    document.getElementById('types').innerHTML = "";
+    console.log(type);
+    type.forEach(function(elem){
+        console.log(elem);
+        let op = document.createElement('option');
+        op.innerHTML = elem;
+        op.id = elem;
+        op.value = elem;
+        document.getElementById('types').append(op);
+    })
+
+    //updates session log
+    document.getElementById('list').innerHTML = "";
+    sessions.forEach(function(elem){
+        console.log(elem);
+        let li = document.createElement('li');
+        li.innerHTML = elem.toString();
+        document.getElementById('list').append(li);
+    })
+}
+
+//session object
+let session = function(inputTime,inputType){
+    let objTime = inputTime;
+    let objType = inputType;
+    function toString(){
+        return objTime/1000 + "s, " + objType; 
+    }
+
+    this.toString = toString;
+}
+
+//adds a session to session array 
+function mark(){
+    let inputType = document.getElementById("types").value;
+    sessions.push(new session(clock,inputType));
+    reset();
+    render();
+}
+
+//important to start the page, it sets up the stopwatch and it populates the page
+reset();
+render();
+
+
+//binding events and functions to buttons
+document.getElementById('start').addEventListener("click",start);
+document.getElementById('stop').addEventListener("click",stop);
+document.getElementById('reset').addEventListener("click",reset);
+document.getElementById('mark').addEventListener("click",mark);
+document.getElementById('types').addEventListener("click",createNewType);
+
